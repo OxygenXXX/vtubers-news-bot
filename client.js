235624fea2daf7500c8ac6954a23b701d48e029f
OxygenXXX <oxyyygen.developer@gmail.com>
2022-11-20 20:27:06 +0300

    Added GET request handler for root address

--- client.js
+++ client.js
@@ -4,6 +4,11 @@ const default_port = 3000;
 
 const application = express();
 
+application.get((request, response) =>
+{
+    response.send("<h1>Hello, world!</h1>");
+});
+
 const server = application.listen(default_port, () =>
 {
     console.log(`Port: ${server.address().port}`);
